2787cdcc4ea814e9531283b4c74bf93d99fd299e
BlueBlueSkyZZ <493813966@qq.com>
2017-07-27 21:27:52 +0800

    初步完成http请求，并解决动画播放问题

--- app/src/main/java/com/example/mzy/mzydriftbottle/MainActivity.java
+++ app/src/main/java/com/example/mzy/mzydriftbottle/MainActivity.java
@@ -15,6 +15,18 @@ import android.view.animation.BounceInterpolator;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
 
 
@@ -96,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
 
         AnimatorSet set = new AnimatorSet();
         set.playTogether(animator1,animator2,animator3);
-        set.setDuration(1000);
+        set.setDuration(618);
         set.start();
 
         //动画结束，设置flag
@@ -117,25 +129,24 @@ public class MainActivity extends AppCompatActivity {
         text1 = (TextView) findViewById(R.id.textView);
         text2 = (TextView) findViewById(R.id.textView2);
 
-        drift.setVisibility(View.VISIBLE);
-        get.setVisibility(View.VISIBLE);
-        board.setVisibility(View.VISIBLE);
-        text1.setVisibility(View.VISIBLE);
-        text2.setVisibility(View.VISIBLE);
+
 
         ObjectAnimator animator1 = ObjectAnimator.ofFloat(voice, "scaleX", 1f, 0f, 0f);
         ObjectAnimator animator2 = ObjectAnimator.ofFloat(voice, "scaleY", 1f, 0f, 0f);
         ObjectAnimator animator3 = ObjectAnimator.ofFloat(voice, "translationY", 0f, 100f);
-        ObjectAnimator animator4 = ObjectAnimator.ofFloat(drift, "alpha", 0f, 1f, 1f);
-        ObjectAnimator animator5 = ObjectAnimator.ofFloat(get, "alpha", 0f, 1f, 1f);
-        ObjectAnimator animator6 = ObjectAnimator.ofFloat(board, "alpha", 0f, 1f, 1f);
-        ObjectAnimator animator7 = ObjectAnimator.ofFloat(text1, "alpha", 0f, 1f, 1f);
-        ObjectAnimator animator8 = ObjectAnimator.ofFloat(text2, "alpha", 0f, 1f, 1f);
+        final ObjectAnimator animator4 = ObjectAnimator.ofFloat(drift, "alpha", 0f, 1f, 1f);
+        final ObjectAnimator animator5 = ObjectAnimator.ofFloat(get, "alpha", 0f, 1f, 1f);
+        final ObjectAnimator animator6 = ObjectAnimator.ofFloat(board, "alpha", 0f, 1f, 1f);
+        final ObjectAnimator animator7 = ObjectAnimator.ofFloat(text1, "alpha", 0f, 1f, 1f);
+        final ObjectAnimator animator8 = ObjectAnimator.ofFloat(text2, "alpha", 0f, 1f, 1f);
 
         AnimatorSet set = new AnimatorSet();
-        set.playTogether(animator1,animator2,animator3,animator4,animator5,animator6,animator7,animator8);
-        set.setDuration(1000);
+        set.playTogether(animator1,animator2,animator3);
+        set.setDuration(618);
         set.start();
+
+
+
 
         set.addListener(new AnimatorListenerAdapter() {
             @Override
@@ -144,6 +155,15 @@ public class MainActivity extends AppCompatActivity {
                 //显示录音图标
                 voice.setVisibility(View.INVISIBLE);
 
+                drift.setVisibility(View.VISIBLE);
+                get.setVisibility(View.VISIBLE);
+                board.setVisibility(View.VISIBLE);
+                text1.setVisibility(View.VISIBLE);
+                text2.setVisibility(View.VISIBLE);
+                AnimatorSet set2 = new AnimatorSet();
+                set2.playTogether(animator4,animator5,animator6,animator7,animator8);
+                set2.setDuration(618);
+                set2.start();
             }
         });
 
@@ -165,11 +185,117 @@ public class MainActivity extends AppCompatActivity {
                     voice_flag = true;
                     driftBottle(v);
                 }
-
                 return false;
             }
         });
     }
 
+    private String postDriftUrl = "http://www.iotesta.cn:8080/StrutsTest/postkeyandvalue";
 
+    /**
+     * 扔漂流瓶，创建房间，同时给搭建的后台服务器传输创建的房间
+     * @param createUserId 创建者
+     * @param isDisByUse 是否选择听后销毁
+     * @param postDriftUrl 发送到的服务器地址
+     * response:成功返回频道名，失败返回false
+     */
+    public void postDrift(String createUserId, String isDisByUse, String postDriftUrl) {
+        OkHttpClient client = new OkHttpClient();
+        FormBody formBody = new FormBody.Builder()
+                .add("CreateUserId", createUserId)
+                .add("IsDisByUse", isDisByUse)
+                .build();
+        Request request = new Request.Builder()
+                .url(postDriftUrl)
+                .post(formBody)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d("postDrift", "onFailure:回调失败 ");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String result = response.body().string();
+                Log.d("postDrift", "onResponse() returned: " +  result);
+            }
+        });
+    }
+
+    private String getGetBottleUrl = "";
+
+    /**
+     * 捡一个漂流瓶
+     * @param getGetBottleUrl 捡漂流瓶的服务器地址
+     * response:JSON格式 Name 频道名
+     *                   IsDisByUse 是否选择听后销毁 0否 1是
+     */
+    public void getGetBottle(String getGetBottleUrl){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .get()
+                .url(getGetBottleUrl)
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+                Log.d("getBottle", "onFailure: 回调失败");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String result = response.body().string();
+                Log.d("getBottle", "onResponse() returned: " +  result);
+                try {
+                    JSONObject jsonObject = new JSONObject(result);
+                    String roomName = jsonObject.getString("Name");
+                    String isDisByUse = jsonObject.getString("IsDisByUse");
+                    Log.d("getBottle", "name = " + roomName + ", isDisByUse = " + isDisByUse);
+
+                    if(isDisByUse == "0"){
+                        //不销毁房间
+                    }else if(isDisByUse == "1"){
+                        //销毁房间
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * 创建频道方法
+     * @param selfId
+     * @param channelName
+     * @param pw
+     */
+    public void myCreateChannel(int selfId, String channelName, String pw){
+
+        //createPublicChannel(selfId, channelName, pw);
+    }
+
+
+    /**
+     * 删除频道方法
+     * @param uid
+     * @param type
+     * @param channelId
+     */
+    public void myDeleteChannel(int uid, String type, String channelId){
+        //deletePublicChannel(uid, type, channelId);
+
+    }
+
+    /**
+     * 进入频道方法
+     */
+    public void myJoinChannel(){
+
+    }
 }
